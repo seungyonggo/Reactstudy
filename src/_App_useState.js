b64da00a01b99a1212e3358873f63db77bd2ddd3
seingyonggo <tmddyd432@gmail>
2023-05-03 17:28:53 +0900

    'one'

---
+++ src/_App_useState.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+
+const heavyWork = () => {
+  console.log("엄청 무거운 작업!!!");
+  return ["홍길동", "김민수"];
+};
+
+function App() {
+  const [names, setName] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setName((prevState) => {
+      console.log("이전 state: ", prevState);
+      return [input, ...prevState];
+    });
+  };
+
+  return (
+    <>
+      <input type="text" value={input} onChange={handleInputChange}></input>
+      <button onClick={handleUpload}>Update</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
+    </>
+  );
+}
+
+export default App;
