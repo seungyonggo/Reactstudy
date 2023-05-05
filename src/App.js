4539cf4b50e9224b7a878dcecba5b428d1f0e5a9
seingyonggo <tmddyd432@gmail>
2023-05-05 15:25:51 +0900

    'two'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import React, { useCallback, useState } from "react";
-import Box from "./Box";
+import React, { useState } from "react";
 
 function App() {
-  const [size, setSizes] = useState(100);
-  const [isDark, setIsDark] = useState(false);
-
-  const createBoxStyle = useCallback(() => {
-    return {
-      backgroundColor: "pink",
-      width: `${size}px`,
-      height: `${size}px`,
-    };
-  }, [size]);
-  return (
-    <div
-      style={{
-        background: isDark ? "black" : "white",
-      }}
-    >
-      <input
-        type="number"
-        value={size}
-        onChange={(e) => setSizes(e.target.value)}
-      />
-      <button onClick={() => setIsDark(!isDark)}>Change Theme</button>
-      <Box createBoxStyle={createBoxStyle} />
-    </div>
-  );
+  return <></>;
 }
-
 export default App;
---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+
+const Child = ({ name, tellMe }) => {
+  console.log("👶자녀도 렌더링이 되었네요");
+  return (
+    <div style={{ border: "2px solid powderblue", padding: "10px" }}>
+      <h3>👶자녀</h3>
+      <p>이름: {name}</p>
+      <button onClick={tellMe}>엄마 나 사랑해?</button>
+    </div>
+  );
+};
+
+export default memo(Child);
