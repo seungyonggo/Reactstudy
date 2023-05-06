62c50e9284ec58fd83c4336da4182742fc0dde87
seingyonggo <tmddyd432@gmail>
2023-05-06 17:14:16 +0900

    '23.05.06 공부'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  return <></>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange} />
+      <button>확인</button>
+    </div>
+  );
 }
 export default App;
